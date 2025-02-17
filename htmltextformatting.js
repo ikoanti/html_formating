b555bfd8e0e291c801e01c353fefa3840532c806
Irakli <irakli_antidze@yahoo.com>
2025-02-17 22:30:55 +0400

    Update htmltextformatting.js
    Use a function inside editor.addButton() to correctly reference this.text().
    
    Check if the selected text already has a formatting tag before wrapping.
    
    Use editor.undoManager.transact() to ensure actions are reversible.

--- htmltextformatting.js
+++ htmltextformatting.js
@@ -1,14 +1,25 @@
 (function() {
     tinymce.PluginManager.add('htmltextformatting', function(editor, url) {
         var formattingTags = ['underline', 'unlink', 'sub', 'sup'];
-        for (var i = 0; i < formattingTags.length; i++) {
-            editor.addButton(formattingTags[i], {
-                text: formattingTags[i].toUpperCase(),
+
+        formattingTags.forEach(function(tag) {
+            editor.addButton(tag, {
+                text: tag.toUpperCase(),
                 icon: false,
                 onclick: function() {
-                    editor.execCommand('mceInsertContent', false, '<' + this.text() + '>' + editor.selection.getContent() + '</' + this.text() + '>');
+                    var selectedText = editor.selection.getContent({ format: 'html' });
+
+                    if (!selectedText) return;
+
+                    editor.undoManager.transact(function() {
+                        editor.execCommand(
+                            'mceInsertContent',
+                            false,
+                            `<${tag}>${selectedText}</${tag}>`
+                        );
+                    });
                 }
             });
-        }
+        });
     });
 })();
